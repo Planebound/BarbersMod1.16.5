497b3fc3d5b7de5055693eaa5a92b13e174eaaaa
nootascendant <bbarber102@gmail.com>
2021-09-04 13:34:36 -0400

    update 1.1
    imbued berry was played with
    fortune ores only have 75% chance of dropping

--- src/main/java/io/github/nootascendant/barbersmod/common/items/SpecialFoods.java
+++ src/main/java/io/github/nootascendant/barbersmod/common/items/SpecialFoods.java
@@ -7,9 +7,9 @@ import net.minecraft.potion.Effects;
 public class SpecialFoods {
 
 	public static final Food IMBUED_BERRY = new Food.Builder().effect(
-			() -> new EffectInstance(Effects.MOVEMENT_SPEED, 100, 1), 1.0f)
-			.nutrition(2)
-			.saturationMod(0.2f)
+			() -> new EffectInstance(Effects.MOVEMENT_SPEED, 200, 3), 1.0f)
+			.nutrition(0)
+			.saturationMod(0.0f)
 			.alwaysEat()
 			.fast()
 			.build();
